9e0cc5fbb2fcaf46a997bee5c6a4af347dcb19e9
Caio Martins <caio.martins176@gmail.com>
2022-05-09 15:45:15 -0300

    dividindo as funcionalidades da aplicação

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,30 @@
-export function SideBar() {
-  // Complete aqui
+import { useState } from "react";
+import { GenreResponseProps } from "../App";
+import { Button } from "./Button";
+
+interface Props {
+  genresData: GenreResponseProps[];
+  onHandleClickButton: (id: number) => void;
+  selectedGenreId: number;
 }
+
+export function SideBar({ genresData, onHandleClickButton, selectedGenreId }: Props) {
+  return (
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {genresData.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => onHandleClickButton(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+      </nav>
+    </div>
+)}
